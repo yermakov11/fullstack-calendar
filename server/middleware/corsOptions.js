504efba97014faa58eaed9007dc5be80afabc188
Dmytro Yermakov <thumpel81@gmail.com>
2026-03-08 21:25:52 +0100

    removed cors

--- server/middleware/corsOptions.js
+++ server/middleware/corsOptions.js
@@ -1,22 +1,7 @@
-const allowedOrigins = [
-  'http://localhost:5173',
-  'https://fullstack-calendar-chi.vercel.app',
-  'https://fullstack-calendar-sigma.vercel.app',
-  'https://fullstack-calendar-gpio82ryc-yermakov11s-projects.vercel.app',
-];
-
 const corsOptions = {
-  origin: (origin, callback) => {
-    if (allowedOrigins.includes(origin) || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error(`CORS policy: origin ${origin} is not allowed`));
-    }
-  },
+  origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type'],
-  credentials: true,
-  optionsSuccessStatus: 200
 };
 
 module.exports = corsOptions;
