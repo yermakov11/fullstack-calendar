332af646a7c84ba66154907d7960ccf790172dc4
Dmytro Yermakov <thumpel81@gmail.com>
2026-03-08 20:59:59 +0100

    removed cors

--- server/middleware/corsOptions.js
+++ server/middleware/corsOptions.js
@@ -1,22 +1,22 @@
-const allowedOrigins = [
-  'http://localhost:5173',
-  'https://fullstack-calendar-chi.vercel.app',
-  'https://fullstack-calendar-sigma.vercel.app',
-  'https://fullstack-calendar-gpio82ryc-yermakov11s-projects.vercel.app',
-];
+// const allowedOrigins = [
+//   'http://localhost:5173',
+//   'https://fullstack-calendar-chi.vercel.app',
+//   'https://fullstack-calendar-sigma.vercel.app',
+//   'https://fullstack-calendar-gpio82ryc-yermakov11s-projects.vercel.app',
+// ];
   
-  const corsOptions = {
-    origin: (origin, callback) => {
-      if (allowedOrigins.includes(origin) || !origin) {
-        callback(null, true);
-      } else {
-        callback(new Error(`CORS policy: origin ${origin} is not allowed`));
-      }
-    },
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type'],
-    credentials: true,        
-    optionsSuccessStatus: 200
-  };
+//   const corsOptions = {
+//     origin: (origin, callback) => {
+//       if (allowedOrigins.includes(origin) || !origin) {
+//         callback(null, true);
+//       } else {
+//         callback(new Error(`CORS policy: origin ${origin} is not allowed`));
+//       }
+//     },
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//     allowedHeaders: ['Content-Type'],
+//     credentials: true,        
+//     optionsSuccessStatus: 200
+//   };
   
-  module.exports = corsOptions;
+//   module.exports = corsOptions;
